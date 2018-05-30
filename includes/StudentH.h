ab06bcc7fe4e4e1b1f51a7908600229255651611
JonasSiuksteris <jonasjonas32@gmail.com>
2018-05-31 00:30:52 +0300

    v0.2
    Sutvarkytas readme

--- includes/StudentH.h
+++ includes/StudentH.h
@@ -3,7 +3,9 @@
 
 class Student{
     private:
-        // Realizacija
+        ///
+        /// Realizacija
+        ///
         std::string name_;
         std::string surname_;
         std::vector<double> Marks_;
@@ -12,41 +14,75 @@ class Student{
         double median_;
         std::string category_;
     public:
-        // Inicializuojama
+        ///
+        /// Inicializuojama
+        ///
         Student() : exam_(0) {}
-        // Studento vardas
+        ///
+        /// Studento vardas
+        ///
         void SetName(std::string);
-        // Studento pavarde
+        ///
+        /// Studento pavarde
+        ///
         void SetSurname(std::string);
-        // Studento egzamino pažymys
+        ///
+        /// Studento egzamino pažymys
+        ///
         void SetExam(double);
-        // Namų darbų pažymys
+        ///
+        /// Namų darbų pažymys
+        ///
         void AddMark(double);
-        // Pažymių rūšiavimas didėjimo tvarka
+        ///
+        /// Pažymių rūšiavimas didėjimo tvarka
+        ///
         void SortMarks();
-        // Balas pagal vidurkį
+        ///
+        /// Balas pagal vidurkį
+        ///
         void Average();
-        // Balas pagal medianą
+        ///
+        /// Balas pagal medianą
+        ///
         void Median();
-        // Kategorija(vargšiukai ir kietiakai)
+        ///
+        /// Kategorija(vargšiukai ir kietiakai)
+        ///
         void Category();
-        // Gražinamas vardas
+        ///
+        /// Gražinamas vardas
+        ///
         inline std::string getName() const { return name_; }
-        // Gražinama pavardė
+        ///
+        /// Gražinama pavardė
+        ///
         inline std::string getSurname() const {return surname_; }
-        // Grąžinami pažymiai
+        ///
+        /// Grąžinami pažymiai
+        ///
         inline std::vector<double> getMarks() const { return Marks_; }
-        // Gražinamas egzamino pažymys
+        ///
+        /// Gražinamas egzamino pažymys
+        ///
         inline double getExam() const { return exam_; }
-        // Grąžinamas vidurkis
+        ///
+        /// Grąžinamas vidurkis
+        ///
         inline double getAverage() const { return average_; }
-        // Grąžinama mediana
+        ///
+        /// Grąžinama mediana
+        ///
         inline double getMedian() const { return median_; }
-        // Gražinama kategorija
+        /// Gražinama kategorija
+        ///
         inline std::string getCategory() const { return category_; }
-        // Pagal varda žemiau
-         bool operator< (const Student& x);
-        // Pagal vardą aukščiau
+        /// Pagal varda žemiau
+        ///
+        bool operator< (const Student& x);
+        ///
+        /// Pagal vardą aukščiau
+        ///
         bool operator> (const Student& x);
 
 };
